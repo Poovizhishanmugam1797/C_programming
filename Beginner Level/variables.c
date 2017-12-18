118ecaa83bff057b2a79db92eb3cffd8de3fd915
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2017-12-18 14:12:03 +0530

    Create variables.c

---
+++ Beginner Level/variables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+char c[30]="Sachin Tendulkar";
+char x[30]="Little Master";
+int a=18426;
+float f=44.83;
+int b=463;
+printf("%s",c);
+printf("\n%s",x);
+printf("\n%d",a);
+printf("\n%f",f);
+printf("\n%d",b);
+return 0;
+}
