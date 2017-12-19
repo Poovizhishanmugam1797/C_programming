09b6b32c3598cdfc48588953b9a8ec9f4564ae74
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2017-12-19 22:18:02 +0530

    Create ODD.c

---
+++ CODEKATA-C/SET-1/ODD.c
@@ -0,0 +1,14 @@
+// Num is even/odd
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	   printf("Input  : ");
+	scanf("%d",&n);
+	if(n%2==0)
+	   printf("Output : Even ");
+	else
+	   printf("Output : Odd  ");
+	   return 0;
+}//main
+	       
