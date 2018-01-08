2340134fd4c9cb3a2284459d7cc7cb28f010883d
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-01-08 19:39:15 +0530

    Create POW.java

---
+++ CODEKATA-C/SET 2/POW.java
@@ -0,0 +1,30 @@
+package CODEKATTAA;
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+class t1
+{
+      Scanner scan=new Scanner(System.in);
+      void getd()
+      {
+    	  int n1=scan.nextInt();int n2=scan.nextInt();
+    	    
+    	System.out.print(Math.pow(n1, n2));
+    	
+      }//getd
+}//class
+  
+public class power 
+{
+
+	
+	public static void main(String[] args)
+	
+	{
+		t1 p=new t1();
+		p.getd();
+		
+
+	}
+
+}
