425a256a325d222d85b03274979dc8ba733f3b81
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-01-08 19:22:13 +0530

    Create countdigits.java

---
+++ CODEKATA-C/SET-1/countdigits.java
@@ -0,0 +1,28 @@
+package CODEKATTAA;
+import java.io.*;
+import java.util.Scanner;
+
+class t
+{
+      Scanner scan=new Scanner(System.in);
+      void getd()
+      {
+    	  String s=scan.nextLine();
+    	  int n=s.length();
+    	  System.out.print(n);
+      }//getd
+}//class
+    	  
+    	  
+   public class count 
+
+{
+
+	public static void main(String[] args)
+	
+	{
+		t p=new t();
+		p.getd();
+	}
+
+}
