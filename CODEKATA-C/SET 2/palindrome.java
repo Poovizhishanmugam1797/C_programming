0f1fe42110f5a51975729364a78faf867929fbcf
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-01-08 19:41:15 +0530

    Create palindrome.java

---
+++ CODEKATA-C/SET 2/palindrome.java
@@ -0,0 +1,36 @@
+package CODEKATTAA;
+import java.io.*;
+import java.util.Scanner;
+class E
+{
+	Scanner scan=new Scanner(System.in);
+	void getd()
+	{
+		String s=scan.nextLine();
+		
+		StringBuilder b=new StringBuilder(s);
+		String s1=b.reverse().toString();
+		System.out.print(s1);
+		if(s1.equals(s))
+		{
+			System.out.print("\nPalindrome");
+		}
+		else
+			System.out.print("\nNot Palindrome");
+	}//getd
+}//class
+		
+		
+public class reverse 
+{
+
+	public static void main(String[] args)
+	
+	{
+		
+    E p=new E();
+    p.getd();
+    
+	}
+
+}
