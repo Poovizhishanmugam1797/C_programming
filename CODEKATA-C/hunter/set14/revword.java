b489a0950407295619997fa4c538951deed43376
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-01-18 23:18:01 +0530

    Create revword.java

---
+++ CODEKATA-C/hunter/set14/revword.java
@@ -0,0 +1,40 @@
+package CODEKATTAA;
+import java.util.ArrayList;
+import java.util.Scanner;
+class R1
+{
+	Scanner scan=new Scanner(System.in);
+	void getd()
+	{
+		String s1=scan.nextLine();
+		String s2[]=s1.split(" ");
+		if(s1.length()>0)
+		{
+		for(int i=(s2.length-1);i>=0;i--)
+		{
+			System.out.print(s2[i]+" ");
+		}
+		}
+		else
+			System.out.print("null");
+		
+		
+				
+		
+		
+	}//getd
+}//class
+		
+public class reverse1
+{
+
+	
+	public static void main(String[] args) 
+	{
+		
+      R1 p=new R1();
+      p.getd();
+      
+	}
+
+}
