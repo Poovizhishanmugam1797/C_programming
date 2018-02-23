01c2ffce0cbe7752af0ae073a80cd64f3ff65349
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-02-23 14:56:37 +0530

    Create COUNT.java

---
+++ Beginner Level/SET 12/COUNT.java
@@ -0,0 +1,21 @@
+package cdekatta;
+import java.util.*;
+class C
+{
+	Scanner scan=new Scanner(System.in);
+	String s;
+	void getd()
+	{
+		s=scan.nextLine();
+		int n=s.length();
+		System.out.print(n);
+	}
+}
+public class countdigit 
+{
+	public static void main(String args[])
+	{
+  C p=new C();
+  p.getd();
+	}
+}
