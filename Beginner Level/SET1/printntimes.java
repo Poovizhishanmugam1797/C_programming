73942cf3f7ee334a84771784c5d859ee3687b282
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-02-23 11:40:38 +0530

    Create printntimes.java

---
+++ Beginner Level/SET1/printntimes.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class P
+{
+	Scanner scan=new Scanner(System.in);
+	void getd()
+	{
+		int n=scan.nextInt();
+		for(int i=0;i<n;i++)
+			System.out.print("Hello"+"\n");
+		
+	}//getd
+}
+public class SET1 
+{
+	
+	public static void main(String[] args)
+	{
+		P p=new P();
+		p.getd();
+
+	}
+
+}
